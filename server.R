d48830fc06ef325b9776cef4e29f2594286344cd
Klaus-Dieter Schmatz <kschmatz@gmail.com>
2014-06-08 10:10:19 +0200

    Refactored server.R

--- server.R
+++ server.R
@@ -2,17 +2,14 @@ library(shiny)
 library(ggplot2)
 data(mtcars)
 
-lm <- lm(mpg ~ cyl + wt, data = mtcars)
+lm <- lm(mpg ~ wt + factor(cyl), data = mtcars)
 fig <- ggplot(mtcars, aes(wt, mpg)) + geom_point() + facet_wrap(~ cyl) +
   xlab("Weight (1000 lbs)") + ylab("MPG")
 
 shinyServer(  
   function(input, output) {
-    output$cyl <- renderText(input$cyl)
-    output$wt <- renderText(input$wt)
     output$mpg <- renderText({
-      predict(lm, data.frame(cyl = as.numeric(as.character(input$cyl)),
-                             wt = input$wt / 1000))
+      predict(lm, data.frame(cyl = input$cyl, wt = input$wt / 1000))
     })
     output$fig <- renderPlot(print(fig))
   }
